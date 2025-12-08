5376d7ad7cabb8751b35d0d257b34e09d3471dcf
= <=>
2025-12-08 12:30:14 +0300

    Added Book class and getters

---
+++ lab_5-6/src/Book.cpp
@@ -0,0 +1,92 @@
+#include "Book.h"
+
+Book::Book() 
+    : title(""), author(""), year(MIN_YEAR), isbn(""), isAvailable(true), borrowedBy("")
+{
+}
+
+Book::Book(const std::string& title, const std::string& author, const int& year_, const std::string& isbn)
+    : title(title), author(author), isbn(isbn), isAvailable(true), borrowedBy("")
+{
+    const auto now = std::chrono::system_clock::now();
+    const auto today = std::chrono::floor<std::chrono::days>(now);
+    const std::chrono::year_month_day ymd{today};
+
+    if (ymd.year() < year_ || year_ < MIN_YEAR) {
+        throw std::runtime_error("Invalid Book year!");
+    }
+
+    year = year_;
+}
+
+Book::Book(const Book& other)
+{
+    title = other.title;
+    author = other.author;
+    year = other.year;
+    isbn = other.isbn;
+    // Новая книга, никто не владеет
+    isAvailable = true;
+    borrowedBy = "";
+}
+
+const std::string& Book::getTitle() const
+{
+    return title;
+}
+
+std::string& Book::getTitle()
+{
+    return title;
+}
+
+const std::string& Book::getAuthor() const
+{
+    return author;
+}
+
+std::string& Book::getAuthor()
+{
+    return author;
+}
+
+const int& Book::getYear() const
+{
+    return year;
+}
+
+// Опасный метод, нужна проверка на выход за [1450, cur_year]
+int& Book::getYear()
+{
+    return year;
+}
+
+const std::string& Book::getISBN() const
+{
+    return isbn;
+}
+
+std::string& Book::getISBN()
+{
+    return isbn;
+}
+
+const bool& Book::getIsAvailable() const
+{
+    return isAvailable;
+}
+
+bool& Book::getIsAvailable()
+{
+    return isAvailable;
+}
+
+const std::string& Book::getBorrowedBy() const
+{
+    return borrowedBy;
+}
+
+std::string& Book::getBorrowedBy()
+{
+    return borrowedBy;
+}
---
+++ lab_5-6/src/Book.h
@@ -0,0 +1,46 @@
+#include <string>
+#include <exception>
+#include <chrono>
+#include <format>
+
+#define MIN_YEAR 1450
+
+class Book {
+    std::string title;
+    std::string author;
+    int year;
+    std::string isbn;
+
+    bool isAvailable;
+    
+    std::string borrowedBy;
+public:
+    // Конструкторы класса
+    Book();
+    Book(const std::string& title, const std::string& author, const int& year, const std::string& isbn);
+    Book(const Book& other);
+
+    // Getter-методы для закрытых полей класса
+    const std::string& getTitle() const;
+    std::string& getTitle();
+
+    const std::string& getAuthor() const;
+    std::string& getAuthor();
+
+    const int& getYear() const;
+    int& getYear();
+
+    const std::string& getISBN() const;
+    std::string& getISBN();
+
+    const bool& getIsAvailable() const;
+    bool& getIsAvailable();
+
+    const std::string& getBorrowedBy() const;
+    std::string& getBorrowedBy();
+
+    // Функциональные методы
+    void borrowBook(const std::string& userName);
+    void returnBook();
+    void displayInfo();
+}
